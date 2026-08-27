52e12342ea2e8f36f8419b06df424c1c5f823d09
Scheidt <pedrohenriqueprazeres@hotmail.com>
2026-08-26 23:46:45 -0300

    cleanup: removed legacy files

--- challenge/segunda_etapa/1A.py
+++
@@ -1,6 +0,0 @@
-"""
-Criar uma função em sua linguagem preferida. A função deve receber um número N >= 0 (deve
-validar o input para a função), e retornar o valor correspondente deste número na sequência
-Fibonacci. EX. ﬁb(0) =0; ﬁb(1) = 1; ﬁb(2) = 1; ﬁb(3) = 2; ﬁb(5) = 5; ﬁb(6) = 8.
--- Criar uma função recursiva que resolva Fibonacci
-"""
--- challenge/segunda_etapa/2B.py
+++
@@ -1,6 +0,0 @@
-"""
-Criar uma função em sua linguagem preferida. A função deve receber um número N > 1 (validar
-o input), e retornar todos os números primos até o número N. EX. p(2) = [2]; p(3) = [2, 3]; p(10) = [2,
-3, 5, 7];
--- Criar uma função iterativa que resolva p
-"""
